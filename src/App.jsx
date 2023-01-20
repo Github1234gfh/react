6c70cd4c893c39e4c41ae63d9255166b62518cde
Github1234gfh <83724836+Github1234gfh@users.noreply.github.com>
2023-01-20 18:27:38 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 
 // import './App.css';
 import React from "react";
-import Fiveth_task from "./Components/Fiveth_task";
 
 import First_task from "./Components/First_task";
-import Fourth_task from "./Components/Fourth_task";
-import Second_task from "./Components/Second_task";
-import Third_task from "./Components/Third_task";
 
 function App() {
   const a = '-'.repeat(100)
